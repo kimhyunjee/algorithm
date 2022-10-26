823d48cf992707a11b182d805035e2375b969b50
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2022-10-26 15:18:28 +0900

    [Silver IV] Title: 로프, Time: 352 ms, Memory: 24076 KB -BaekjoonHub

---
+++ 백준/Silver/2217. 로프/로프.js
@@ -0,0 +1,33 @@
+function solution(n, list) {
+	// js 정렬방법
+    list.sort(function(a, b) {
+        return a - b;
+    })
+
+    let result = 0;
+    //로프중 최대값 찾기
+    for(let i=0; i<n; i++){
+        if(result < list[i] * (n-i)){
+            result = list[i] * (n-i);
+        }
+    }
+    console.log(result);
+}
+
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+let input = [];
+rl.on("line", function (line) {
+    //여러줄 입력
+    input.push(line)
+}).on("close", function () {
+
+    input = input.map((el) => parseInt(el));
+    let n = input[0];
+    let list = input.slice(1);
+    solution(n, list);
+});
