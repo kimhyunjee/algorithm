15214d33cf5705dccf944b45d8c07bc89b59d5de
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2022-11-23 02:00:52 +0900

    [Bronze II] Title: 손익분기점, Time: 116 ms, Memory: 9600 KB -BaekjoonHub

---
+++ 백준/Bronze/1712. 손익분기점/손익분기점.js
@@ -0,0 +1,8 @@
+const fs = require('fs');
+const input = fs.readFileSync("/dev/stdin").toString().trim().split(' ').map(Number);
+
+const [A, B, C] = [input[0], input[1], input[2]];
+
+let a = Math.floor(A/(C-B)) + 1;
+
+B >= C ? console.log(-1) : console.log(a);
