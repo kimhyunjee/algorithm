0fe7598852e46441bfccb429e5dc3cc7d7554b4f
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-03-29 02:43:36 +0900

    Create N과 M(3).js

---
+++ 백준/Silver/N과 M(3).js
@@ -0,0 +1,22 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split(' ').map(nm => parseInt(nm));
+
+const N = input.shift();
+const M = input.shift();
+const output = [];
+let result = '';
+
+function dfs(cnt) {
+  if (cnt === M) {
+    result += `${output.join(' ')}\n`;
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    output.push(i + 1);
+    dfs(output.length);
+    output.pop();
+  }
+}
+
+dfs(0);
+console.log(result.trim());
