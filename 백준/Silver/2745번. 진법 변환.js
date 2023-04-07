170ccb170e0d34b5a85ff6893fc60383afcb8c26
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-04-08 04:02:25 +0900

    Create 2745번. 진법 변환.js

---
+++ 백준/Silver/2745번. 진법 변환.js
@@ -0,0 +1,14 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split(' ');
+let char = input[0].split('').reverse();
+let base = Number(input[1]);
+let result = 0;
+for(let i=0; i<char.length; i++){
+    if(char[i] >= 'A' && char[i] <= 'Z'){
+        char[i] = char[i].charCodeAt(0)-55;
+        result += char[i] * Math.pow(base,i);
+    }else{
+        char[i] = Number(char[i]);
+        result += char[i] * Math.pow(base,i);
+    }
+}
+console.log(result);
