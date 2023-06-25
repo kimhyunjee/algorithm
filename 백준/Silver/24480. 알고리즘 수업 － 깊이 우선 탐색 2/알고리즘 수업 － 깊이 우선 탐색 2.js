d7685e6a8472d2cd27549e01183153b4b4540b94
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-06-25 21:43:42 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 844 ms, Memory: 104736 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.js
@@ -0,0 +1,27 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
+
+const [N, M, R] = input.shift().split(' ').map(Number);
+const arr = input.map((v) => v.split(' ').map(Number));
+const graph = [...Array(N + 1)].map(() => []);
+const visited = Array(N).fill(0);
+let cnt = 1;
+
+arr.map(([from, to]) => {
+  graph[from].push(to);
+  graph[to].push(from);
+});
+
+
+graph.map((v) => v.sort((a, b) => b - a));
+
+const dfs = (node) => {
+  if (!visited[node - 1]) {
+    visited[node - 1] = cnt;
+    cnt++;
+    for (const next of graph[node]) dfs(next);
+  }
+};
+
+dfs(R);
+
+console.log(visited.join('\n'));
