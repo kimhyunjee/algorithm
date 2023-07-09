5987126413dd1ef92e531fe0c47d1df7b0c4abd6
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-07-09 23:21:35 +0900

    [Bronze V] Title: 과제 안 내신 분..?, Time: 116 ms, Memory: 9328 KB -BaekjoonHub

---
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.js
@@ -0,0 +1,18 @@
+const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+
+const nums = input.map((num) => Number(num));
+sol(nums);
+
+function sol(nums) {
+  let map = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    map.set(nums[i], 1);
+  }
+
+  for (let i = 1; i <= 30; i++) {
+    if (!map.has(i)) {
+      console.log(i);
+    }
+  }
+}
