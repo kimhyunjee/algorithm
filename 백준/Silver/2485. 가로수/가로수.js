88e9cb44099e680ac3a0542dbda452cc9c7a11d3
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2023-08-14 23:14:39 +0900

    [Silver IV] Title: 가로수, Time: 236 ms, Memory: 24908 KB -BaekjoonHub

--- 백준/Silver/2485. 가로수/가로수.js
+++ 백준/Silver/2485. 가로수/가로수.js
@@ -1,35 +1,33 @@
-const [count, ...strInput] =  require("fs")
-  .readFileSync("/dev/stdin")
-  .toString()
-  .trim()
-  .split("\n");
-const input = strInput.map(Number);
-
-const getGcd = (x, y) => {
-  if (y == 0) return x;
-  else return getGcd(y, x % y);
-};
-
-const dist = [];
-for (let i = 0; i < count - 1; i++) {
-  dist.push(input[i + 1] - input[i]);
+const input = require('fs').readFileSync("./dev/stdin").toString().trim().split("\n").map(v=>+v);
+const N = input.shift();
+const trees = input.sort((a,b)=>a-b);
+let max = 0;
+let interval = [];
+let answer = 0;
+for(let i = 0; i<trees.length-1; i++){
+  const dist = trees[i+1]-trees[i];
+  interval.push(dist)
+  if(max<dist) max = dist;
 }
 
-let gcd = input[count - 1];
-for (let i = 0; i < count - 2; i++) {
-  let tempGcd = getGcd(dist[i + 1], dist[i]);
-  if (tempGcd < gcd) gcd = tempGcd;
-}
-
-
-let output = 0;
-
-for (let i = 0; i < count - 1; i++) {
-  const tempDis = dist[i];
-
-  if (tempDis !== gcd) {
-    output += tempDis / gcd - 1;
+function gcd(a,b){
+  let temp;
+  while(b!=0){
+    temp = a%b;
+    a = b;
+    b = temp;
   }
+  return a;
 }
 
-console.log(output);
+let GCD = gcd(interval[0],interval[1]);
+
+for(let i = 2; i<interval.length; i++){
+  GCD = gcd(GCD,interval[i])
+}
+
+interval.forEach(v=>{
+  if(v>GCD) answer+=(v/GCD)-1
+})
+
+console.log(answer)
