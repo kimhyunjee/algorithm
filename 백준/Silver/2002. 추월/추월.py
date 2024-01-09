cf619a66924e3539dd69353fcbd2646972e02ff0
hyunjee <107829027+kimhyunjee@users.noreply.github.com>
2024-01-09 10:32:37 +0900

    [Silver I] Title: 추월, Time: 148 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Silver/2002. 추월/추월.py
@@ -0,0 +1,20 @@
+N = int(input())
+
+answer = 0
+enter  = dict()
+out = []
+
+for i in range(N):
+    car = input()
+    enter[car] = i
+
+for i in range(N):
+    car = input()
+    out.append(car)
+
+for i in range(N - 1):
+    for j in range(i + 1, N):
+        if enter[out[i]] > enter[out[j]]:
+            answer += 1
+            break
+print(answer)
